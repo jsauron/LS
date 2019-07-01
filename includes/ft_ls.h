31d802b6d06866d84987ef9de043086247fb4d9b
Jsauron <jsauron@student.42.fr>
2019-07-01 19:19:06 +0200

    initialisation+base

---
+++ includes/ft_ls.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsauron <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/01 18:42:46 by jsauron           #+#    #+#             */
+/*   Updated: 2019/07/01 19:06:40 by jsauron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+#include "../libft/libft.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/xattr.h>
+#include <dirent.h>
+
+
+#endif
